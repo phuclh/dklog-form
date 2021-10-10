8fcc7ee3aa2966a0ccbd522393c7f1d9816fdf4a
Phuc Le <phuclh93@gmail.com>
2021-10-09 22:14:50 -0700

    Compile assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,12 @@
 const mix = require('laravel-mix');
 const tailwind = require('tailwindcss');
 
-mix.js("resources/js/plugin.js", "resources/dist/js")
+mix.js("resources/js/plugin.js", "dist/js")
     .postCss("resources/css/plugin.css", "dist/css/plugin.css", [
         require("tailwindcss"),
     ]);
 
-mix.js("resources/js/admin/plugin.js", "resources/dist/js/admin")
+mix.js("resources/js/admin/plugin.js", "dist/js/admin")
     .postCss("resources/css/admin/plugin.css", "dist/css/admin/plugin.css", {
     }, [
         tailwind("./tailwind-admin.config.js")
